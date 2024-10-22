588e9402fa74588c2fa89ab87e156d4ae703e1c2
palak <palak89678@gmail.com>
2024-10-22 01:14:29 -0700

    initial commit

---
+++ public/js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXmJ-pVGNrsO7IAQh9oV0diagNUbWVKXA",
+  authDomain: "ist105midtermexampalak.firebaseapp.com",
+  projectId: "ist105midtermexampalak",
+  storageBucket: "ist105midtermexampalak.appspot.com",
+  messagingSenderId: "923553479677",
+  appId: "1:923553479677:web:7e10fb5cb40fef6f463794"
+};
+
+  
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  
